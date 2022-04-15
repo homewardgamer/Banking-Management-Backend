fd2714d9b6049d22e719ad0b989da291121ce536
Aditya Verma <adityarubbers.av@gmail.com>
2022-04-15 12:39:19 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
