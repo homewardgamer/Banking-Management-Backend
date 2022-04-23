df9e5a1b76bc2d0ca4edd1f21222247045cb43cf
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-24 02:07:22 +0530

    Replaced nodejs starter code with django project starter code: Includes .gitignore update

--- app.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const app = express();
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`);
-});
