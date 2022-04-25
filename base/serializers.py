aff5928352900d6d6e802dcd7f38479d8d5c0a2e
Aditya Verma <adityarubbers.av@gmail.com>
2022-04-25 20:48:47 +0530

    Added validation to the register url

--- base/serializers.py
+++ base/serializers.py
@@ -4,6 +4,15 @@ from base.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        if attrs["is_employee"] and attrs["is_customer"]:
+            raise serializers.ValidationError(
+                {"message": "choose only one role"}, code=400)
+        if not attrs["is_employee"] and not attrs["is_customer"]:
+            raise serializers.ValidationError(
+                {"message": "choose only one role"}, code=400)
+        return super().validate(attrs)
+
     class Meta:
         model = User
         fields = [
