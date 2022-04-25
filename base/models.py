8fe1f745fe227a48f2c64cdeba7bff7cbce10da3
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-25 12:36:58 +0530

    Implemented User model from AbstractUser

--- base/models.py
+++ base/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    address = models.CharField(_("address"), max_length=300, blank=True)
+    dob = models.DateField(_("dob"), null=True)
