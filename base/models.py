fff2212556004b5bcb2e02bc555464da32944022
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-26 18:29:59 +0530

    Removed verbose_name attribute from model fields

--- base/models.py
+++ base/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 class Branch(models.Model):
@@ -11,10 +10,10 @@ class Branch(models.Model):
 
 
 class User(AbstractUser):
-    address = models.CharField(_("address"), max_length=300, blank=True)
-    dob = models.DateField(_("dob"), null=True)
-    is_customer = models.BooleanField(_("is_customer"), default=False)
-    is_employee = models.BooleanField(_("is_employee"), default=False)
+    address = models.CharField(max_length=300, blank=True)
+    dob = models.DateField(null=True)
+    is_customer = models.BooleanField(default=False)
+    is_employee = models.BooleanField(default=False)
     updated_at = models.DateTimeField(auto_now=True)
     branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True)
 
@@ -22,12 +21,8 @@ class User(AbstractUser):
 class Account(models.Model):
     account_id = models.BigAutoField(primary_key=True)
     account_holder = models.ForeignKey(User, on_delete=models.CASCADE)
-    account_type = models.CharField(
-        _("account_type"), max_length=300, default="savings"
-    )
-    balance = models.DecimalField(
-        _("balance"), decimal_places=2, max_digits=10, default=0.0
-    )
+    account_type = models.CharField(max_length=300, default="savings")
+    balance = models.DecimalField(decimal_places=2, max_digits=10, default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
