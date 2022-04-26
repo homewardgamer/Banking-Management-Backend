e8f5b33e7e1245f6cae0ef3c4f8802220d11e839
Aditya Verma <adityarubbers.av@gmail.com>
2022-04-26 11:15:37 +0530

    Added branch account and transactions models in db

--- base/models.py
+++ base/models.py
@@ -3,8 +3,43 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class Branch(models.Model):
+    branch_id = models.BigAutoField(primary_key=True)
+    branch_name = models.CharField(max_length=300)
+    branch_address = models.CharField(max_length=500)
+    branch_code = models.IntegerField(unique=True)
+
+
 class User(AbstractUser):
     address = models.CharField(_("address"), max_length=300, blank=True)
     dob = models.DateField(_("dob"), null=True)
     is_customer = models.BooleanField(_("is_customer"), default=False)
     is_employee = models.BooleanField(_("is_employee"), default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    branch_id = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True)
+
+
+class Account(models.Model):
+    account_id = models.BigAutoField(primary_key=True)
+    account_holder = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_type = models.CharField(
+        _("account_type"), max_length=300, default="savings"
+    )
+    balance = models.DecimalField(
+        _("balance"), decimal_places=2, max_digits=10, default=0.0
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Transactions(models.Model):
+    transaction_id = models.BigAutoField(primary_key=True)
+    r_account = models.ForeignKey(
+        Account, on_delete=models.SET_NULL, null=True, related_name="r_transactions"
+    )
+    s_account = models.ForeignKey(
+        Account, on_delete=models.SET_NULL, null=True, related_name="s_transactions"
+    )
+    type = models.CharField(max_length=300)
+    amount = models.DecimalField(decimal_places=2, max_digits=10, default=0.0)
+    timestamp = models.DateTimeField(auto_now_add=True)
