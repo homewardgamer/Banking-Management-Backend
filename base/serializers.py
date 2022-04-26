089ce784843db6a1658dc5406c67b770fbb81dd2
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-26 23:06:01 +0530

    Applied password hashing on user registration
    Passwords are now stored in hashed format when registering user via API

--- base/serializers.py
+++ base/serializers.py
@@ -1,20 +1,12 @@
-from django.db.models import fields
 from rest_framework import serializers
+from django.contrib.auth import password_validation
+from django.contrib.auth.hashers import make_password
 
 from base.models import *
 
 
 class UserSerializer(serializers.ModelSerializer):
-    def validate(self, attrs):
-        if attrs.get("is_employee", False) and attrs.get("is_customer", False):
-            raise serializers.ValidationError(
-                {"message": "choose only one role"}, code=400
-            )
-        if not attrs.get("is_employee", False) and not attrs.get("is_customer", False):
-            raise serializers.ValidationError(
-                {"message": "choose only one role"}, code=400
-            )
-        return super().validate(attrs)
+    password = serializers.CharField(required=True, write_only=True)
 
     class Meta:
         model = User
@@ -30,6 +22,19 @@ class UserSerializer(serializers.ModelSerializer):
             "is_customer",
             "is_employee",
         ]
+
+    def validate(self, attrs):
+        if attrs.get("is_employee", False) and attrs.get("is_customer", False):
+            raise serializers.ValidationError(
+                {"message": "choose only one role"}, code=400
+            )
+        if not attrs.get("is_employee", False) and not attrs.get("is_customer", False):
+            raise serializers.ValidationError(
+                {"message": "choose only one role"}, code=400
+            )
+        password_validation.validate_password(attrs["password"])
+        attrs["password"] = make_password(attrs["password"])
+        return super().validate(attrs)
 
 
 class TransactionSerializer(serializers.ModelSerializer):
