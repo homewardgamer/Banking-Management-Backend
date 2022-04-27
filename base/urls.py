62755bd1914d2b2b64d0dafafcc48a8369c9e4e5
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-27 18:12:57 +0530

    Added route and view to list all transactions of a customer
    Only customer can view their transactions

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("user/register", user_register_view),
     path("user/delete/<user_id>", user_delete_view),
     path("transaction/new", transaction_add_view),
+    path("transaction/view/all", all_transactions_by_customer),
     path("account/create", account_add_view),
 ]
