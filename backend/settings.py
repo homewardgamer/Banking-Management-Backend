fa980142d959c51bea6a1e9a046b82437ba37ea3
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-10 22:56:34 +0530

    Replaced hardcoded values for DEBUG and SECRET_KEY with environment variables

--- backend/settings.py
+++ backend/settings.py
@@ -12,19 +12,22 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_on_heroku
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z5gzd(g=apad4p0xjkq4rv35ows2+t_c((524by)6=q6f7n21&"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["bankmgmtapi.herokuapp.com", "127.0.0.1:8000"]
 
